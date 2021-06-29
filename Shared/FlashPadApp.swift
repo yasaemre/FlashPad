0b6bde645eca52d0cf27fdd2ad5d399cb4aa411b
Emre Yasa <emreyasa86@gmail.com>
2021-06-28 19:20:30 -0700

    Initial Commit

---
+++ Shared/FlashPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashPadApp.swift
+//  Shared
+//
+//  Created by Emre Yasa on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
