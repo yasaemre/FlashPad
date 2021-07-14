5fa6ad102504ae17e36ef40a3c62d6ea981f1e2d
Emre Yasa <emreyasa86@gmail.com>
2021-07-13 22:12:44 -0700

    Clean the code

--- Shared/HomeView.swift
+++ Shared/HomeView.swift
@@ -11,7 +11,6 @@ struct HomeView : View {
     
     @State var show = false
     //@State var logged = UserDefaults.standard.value(forKey: "logged") as? Bool ?? false
-    
     @AppStorage("logged") var logged = false
     @AppStorage("loggedViaEmail") var loggedViaEmail = ""
 
