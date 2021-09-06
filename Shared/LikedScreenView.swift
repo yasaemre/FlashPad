8b77098a42ade96737ad1c2d84e18bdf6c5adad0
Emre Yasa <emreyasa86@gmail.com>
2021-09-06 15:23:53 -0700

    Create liked screen

---
+++ Shared/LikedScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LikedScreenView.swift
+//  FlashPad
+//
+//  Created by Emre Yasa on 9/6/21.
+//
+
+import SwiftUI
+
+struct LikedScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LikedScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LikedScreenView()
+    }
+}
